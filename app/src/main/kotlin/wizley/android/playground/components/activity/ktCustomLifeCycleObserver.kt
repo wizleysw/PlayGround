ee0ba2b926fcb38bf8f5d0dd6d74dd1aece2346c
wizleysw <wizley@kakao.com>
2020-06-12 21:49:47 +0900

    add LifeCycleActivity and LifeCycleObserver in kotlin version
    Reviewed by Wizley.
    2020.06.12.
    
    kt keyword front of class file indicates that class is written in kotlin language.

---
+++ app/src/main/kotlin/wizley/android/playground/components/activity/ktCustomLifeCycleObserver.kt
@@ -0,0 +1,38 @@
+package wizley.android.playground.components.activity
+
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class ktCustomLifeCycleObserver : LifecycleObserver {
+
+    private val TAG = "LifecycleObserver"
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate(){
+        Log.e(TAG, "onCreate")
+    }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart(){
+        Log.e(TAG, "onStart")
+    }
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume(){
+        Log.e(TAG, "onResume")
+    }
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause(){
+        Log.e(TAG, "onPause")
+    }
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop(){
+        Log.e(TAG, "onStop")
+    }
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy(){
+        Log.e(TAG, "onDestroy")
+    }
+
+}
---
+++ app/src/main/kotlin/wizley/android/playground/components/activity/ktLifeCycleActivity.kt
@@ -0,0 +1,81 @@
+package wizley.android.playground.components.activity
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import wizley.android.playground.R
+import androidx.lifecycle.LifecycleRegistry
+
+class ktLifeCycleActivity : Activity(), LifecycleOwner{
+    private val VALUE = ""
+
+    private var v: String? = null
+    private var btn: Button? = null
+    private var lifecycleRegistry: LifecycleRegistry? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        v = savedInstanceState?.getString(VALUE);
+
+        lifecycleRegistry = LifecycleRegistry(this)
+        lifecycleRegistry!!.addObserver(ktCustomLifeCycleObserver())
+        lifecycleRegistry!!.currentState = Lifecycle.State.CREATED
+
+        setContentView(R.layout.activity_lifecycle);
+
+        btn = findViewById<Button>(R.id.button)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        btn!!.text = savedInstanceState?.getString(VALUE)
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(VALUE, v);
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        lifecycleRegistry!!.currentState = Lifecycle.State.STARTED
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        lifecycleRegistry!!.currentState = Lifecycle.State.RESUMED
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        lifecycleRegistry!!.currentState  = Lifecycle.State.STARTED
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        lifecycleRegistry!!.currentState  = Lifecycle.State.CREATED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        lifecycleRegistry!!.currentState = Lifecycle.State.DESTROYED
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        lifecycleRegistry!!.currentState  = Lifecycle.State.CREATED
+    }
+
+    override fun getLifecycle(): Lifecycle {
+        return this.lifecycleRegistry!!
+    }
+}

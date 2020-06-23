a303eed5556d4a555098c82bf1f5ca984c4e00ab
wizleysw <wizley@kakao.com>
2020-06-23 20:29:05 +0900

    add snackbar in java and kotlin
    Reviewed by Wizley.
    2020.06.23.
    
    snackbar to show info to user can be implemented almost same way with toast.

---
+++ app/src/main/kotlin/wizley/android/playground/snackbar/KTSnackBarActivity.kt
@@ -0,0 +1,34 @@
+package wizley.android.playground.snackbar
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import wizley.android.playground.R
+
+class KTSnackBarActivity : AppCompatActivity(){
+    private val TAG = "KTSnackBarActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snackbar)
+
+        val layout : View = layoutInflater.inflate(R.layout.activity_snackbar, findViewById(R.id.coordinatorLayout))
+
+        val snackbar = Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG)
+        snackbar.setAction("UNDO", UndoListener())
+        snackbar.show();
+
+        // Snackbar.make(findViewById(R.id.coordinatorLayout), "SnackBar", Snackbar.LENGTH_LONG).show()
+
+    }
+
+    class UndoListener : View.OnClickListener{
+        private val TAG = "KTSnackBarActivity"
+
+        override fun onClick(v: View?) {
+            Log.e(TAG,  "OnClick")
+        }
+    }
+}

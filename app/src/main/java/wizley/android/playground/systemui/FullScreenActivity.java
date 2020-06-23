d598fbf1739eaf0aaaf196951fc8c8b0e938454e
wizleysw <wizley@kakao.com>
2020-06-23 16:13:27 +0900

    add examples of chaning system-ui in Java
    Reviewed by Wizley.
    2020.06.23.
    
    Visibility of system ui such as status bar is mutable using decorView class. Here is example written in Java.

---
+++ app/src/main/java/wizley/android/playground/systemui/FullScreenActivity.java
@@ -0,0 +1,74 @@
+package wizley.android.playground.systemui;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import wizley.android.playground.R;
+
+public class FullScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_button);
+        setListener();
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(
+                // if want lean_back disable this flag
+                // lean_back is mode that does not interact heavily with screen
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+
+                // to make content under the system bar not to resize when hide and show
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    private void showSystemUI(){
+        View decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+
+    private void setListener(){
+        View decorView = getWindow().getDecorView();
+
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if((visibility & View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN) == 0){
+                    // system bar is visible
+                }
+                else {
+                    // system bar is invisible
+                }
+            }
+        });
+    }
+}

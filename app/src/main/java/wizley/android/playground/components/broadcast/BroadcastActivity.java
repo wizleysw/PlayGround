9e67376365a88c1f14f30cb6589cc706c9fce0b7
wizleysw <wizley@kakao.com>
2020-07-06 21:00:51 +0900

    add BroadcastReceiver
    Reviewed by Wizley.
    2020.07.06.
    
    BroadcastReceiver is added with asynctask the way of deferring work to other thread as broadcast's work should be completed within time limit of at most 8.

---
+++ app/src/main/java/wizley/android/playground/components/broadcast/BroadcastActivity.java
@@ -0,0 +1,38 @@
+package wizley.android.playground.components.broadcast;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BroadcastActivity extends AppCompatActivity {
+    private MyBroadcastReceiver receiver;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        receiver = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("wizley.android.playground.broadcast");
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent intent = new Intent();
+        intent.setAction("wizley.android.playground.broadcast");
+        intent.putExtra("data", "this is broadcast value");
+        sendBroadcast(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
---
+++ app/src/main/java/wizley/android/playground/components/broadcast/MyBroadcastReceiver.java
@@ -0,0 +1,57 @@
+package wizley.android.playground.components.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    private static final String TAG = "MyBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e(TAG, "onReceive");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Action: ").append(intent.getAction()).append("\n");
+        sb.append("URI: ").append(intent.toUri(Intent.URI_INTENT_SCHEME).toString()).append("\n");
+
+        String log = sb.toString();
+        Log.e(TAG, log);
+
+        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+
+        // if need to execute in other thread use this method
+        final PendingResult pendingResult = goAsync();
+        Task asyncTask = new Task(pendingResult, intent);
+        asyncTask.execute();
+    }
+
+    private static class Task extends AsyncTask<String, Integer, String> {
+        private final PendingResult pendingResult;
+        private final Intent intent;
+
+        private Task(PendingResult pendingResult, Intent intent){
+            this.pendingResult = pendingResult;
+            this.intent = intent;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.e(TAG, "doInBackground");
+
+            Log.e(TAG, (String) intent.getExtras().get("data"));
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            // this should be called to be recycled
+            pendingResult.finish();
+        }
+    }
+
+}
